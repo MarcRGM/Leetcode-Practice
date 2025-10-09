5546fd0b015d7d58aa97c9036323696b358be1f1
MarcRGM <marcrodniemanalo@gmail.com>
2025-10-09 09:05:55 +0800

    Valid Anagram

--- easy/valid_anagram.py
+++ easy/valid_anagram.py
@@ -11,5 +11,17 @@ class Solution(object):
         if len(s) != len(t):
             return False
         
-# one liner answer: collections.Counter(s) == collections.Counter(t)
+        countS, countT = {}, {}
+
+        for i in range(len(s)):
+            countS[s[i]] = 1 + countS.get(s[i], 0) # if the letter is not on the dictionary, give it a value of 0 and add 1
+            countT[t[i]] = 1 + countT.get(t[i], 0)
+
+        for c in countS:
+            if countS[c] != countT.get(c, 0):
+                return False
+        
+        return True
+
+# one liner answers: collections.Counter(s) == collections.Counter(t) AND sorted(s) == sorted(t)
 # collections.Counter() is like a dictionary that counts how many times each element appears in a sequence
