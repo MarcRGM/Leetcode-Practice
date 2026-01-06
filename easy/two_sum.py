82869f39b8cf0298716f0d1428a56183abd9b8e1
MarcRGM <marcrodniemanalo@gmail.com>
2026-01-06 20:24:50 +0800

    Two Sum Solution

---
+++ easy/two_sum.py
@@ -0,0 +1,16 @@
+# Example 1:
+
+# Input: nums = [2,7,11,15], target = 9
+# Output: [0,1]
+# Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+
+class Solution(object):
+    def twoSum(self, nums, target):
+
+        indices_of_sum = {}
+
+        for i, num in nums:
+            if target - num in indices_of_sum:
+                return [indices_of_sum[target - num], i]
+        
+        indices_of_sum = i
