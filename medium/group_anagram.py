ebd110d1ccbf0401c1a1cc6cb82acdf9beb6b385
MarcRGM <marcrodniemanalo@gmail.com>
2026-01-09 12:05:41 +0800

    Add better solutiions

--- medium/group_anagram.py
+++ medium/group_anagram.py
@@ -8,8 +8,39 @@ class Solution(object):
 
         for s in strs:
             sorted_word = ''.join(sorted(s)) # sorted(s) returns a list
+            # tuple(sorted((s)) for immutable key in dictionary
             if sorted_word in words:
                 words[sorted_word].append(s) 
             else: 
                 words[sorted_word] = [s]
+
+            # shorter version
+            # if sorted_word not in words:
+                # words[sorted_word] = []
+            # words[sorted_word].append(s)
+
+
+            # Another solution using .setDefault() function without if else
+            # words.setdefault(sorted_word, []).append(s)
+            # setdefault(key, default) returns the value for key if it exists, 
+            # otherwise it creates the key with default and returns that default value.
+
         return words.values()
+    
+
+# Neetcode's solution
+
+from collections import defaultdict
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        res = defaultdict(list)
+
+        for s in strs:
+            count = [0] * 26 # a ... z
+
+            for c in s:
+                count[ord(c) - ord('a')] += 1
+
+            res[tuple(count)].append(s)
+
+        return res.values()
