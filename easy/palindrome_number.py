054496aafdfa2c88345f773957bf375cfffb8092
MarcRGM <marcrodniemanalo@gmail.com>
2026-01-15 00:00:24 +0800

    Palindrome number straigtforward solution

--- easy/palindrome_number.py
+++ easy/palindrome_number.py
@@ -8,4 +8,19 @@ class Solution(object):
         orig = str(x)
         reverse = orig[::-1]
         return orig == reverse
+# Time complexity = O(n)
+# Space complexity = O(n)
+
+# Revert half of the number solution
+# - Alexander Obregon
+class Solution(object):
+    def isPalindrome(self, x):
+        if x < 0 or (x % 10 == 0 and x != 0):
+            return False
         
+        reverted_number = 0
+        while x > reverted_number:
+            reverted_number = reverted_number * 10 + x % 10
+            x //= 10
+        
+        return x == reverted_number or x == reverted_number // 10
